fa213d46bb775e01e2e4b4667798be528957d3e3
Andrew Eades <andrew.eades@gmail.com>
2018-01-11 18:13:23 +0000

    Fin.

--- ActionExtension/ActionViewController.swift
+++ ActionExtension/ActionViewController.swift
@@ -12,18 +12,6 @@ import MobileCoreServices
 class ActionViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
-
-    private func loadImage(from imageURL: NSSecureCoding?) {
-        OperationQueue.main.addOperation {
-            weak var weakImageView = self.imageView
-
-            if let strongImageView = weakImageView {
-                if let imageURL = imageURL as? URL {
-                    strongImageView.image = UIImage(data: try! Data(contentsOf: imageURL))
-                }
-            }
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,25 +22,30 @@ class ActionViewController: UIViewController {
         
         // For example, look for an image and place it into an image view.
         // Replace this with something appropriate for the type[s] your extension supports.
-        var imageFound = false
-        for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
-            for provider in item.attachments! as! [NSItemProvider] {
-                if provider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
-                    // This is an image. We'll load it, then place it in our image view.
-                    provider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil, completionHandler: { (imageURL, error) in
-                        
-                        self.loadImage(from: imageURL)
-                    })
-                    
-                    imageFound = true
-                    break
-                }
-            }
-            
-            if (imageFound) {
-                // We only handle one image, so stop looking for more.
-                break
-            }
+        
+        guard let extensionItems = self.extensionContext!.inputItems as? [NSExtensionItem] else {
+            self.showAlert(title: "Error", message: "No extension items.")
+            return
+        }
+
+        let attachments = extensionItems
+            .map { $0.attachments }
+            .filter { $0 != nil }
+            .flatMap { $0! }
+        
+        let imageProviders = attachments
+            .map { $0 as? NSItemProvider }
+            .filter { $0 != nil }
+            .map { $0! }
+            .filter { $0.hasItemConformingToTypeIdentifier(kUTTypeImage as String) }
+        
+        guard let provider = imageProviders.first else {
+            self.showAlert(title: "Error", message: "No image provider.")
+            return
+        }
+
+        provider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil) { item, error in
+            self.loadImage(from: item)
         }
     }
 
@@ -68,10 +61,59 @@ class ActionViewController: UIViewController {
         // This template doesn't do anything, so we just echo the passed in items.
         self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
     }
+    
+    
+    private func loadImage(from item: NSSecureCoding?) {
+        let image: UIImage?
+        
+        if let imageURL = item as? URL {
+            image =  UIImage(data: try! Data(contentsOf: imageURL))
+            
+        } else if let theImage = item as? UIImage {
+            image = theImage
+            
+        } else if let data = item as? Data {
+            image = UIImage(data: data)
+            
+        } else {
+            image = nil
+        }
+        
+        guard image != nil else {
+            showAlert(title: "Error", message: "Cannot convert item into an image.")
+            return
+        }
+        
+        OperationQueue.main.addOperation {
+            if let strongImageView = self.imageView
+            {
+                strongImageView.image = image
+            }
+        }
+    }
 }
 
 extension ActionViewController: ConsoleLogging {
     var appID: String {
         return "ActionExtensionDemo"
+    }
+}
+
+extension ActionViewController {
+    private func showAlert(title: String, message: String, shouldCloseExtensionWithAlertDismissal: Bool = true) {
+        
+        let alert = UIAlertController(title: title,
+                                      message: String(message.prefix(40)),
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+            if shouldCloseExtensionWithAlertDismissal {
+                self.done()
+            }
+            
+            self.dismiss(animated: true)
+        })
+        
+        self.present(alert, animated: true)
     }
 }
